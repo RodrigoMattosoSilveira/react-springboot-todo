3f55f7916122502770ec0e9cda7506882960a19d
Rodrigo Silveira <r3850365@gmail.com>
2020-05-09 09:11:00 -0700

    basic-features  -  feature - remove stuff I no longer need

--- src/main/java/com/madronetek/reactspringboot/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.madronetek.reactspringboot;
-
-import org.springframework.data.repository.CrudRepository;
-
-// (1) The repository extends Spring Data Commons' CrudRepository and plugs in the type of the domain object and its primary key
-public interface TodoRepository extends CrudRepository<Todo, Long> {
-}
