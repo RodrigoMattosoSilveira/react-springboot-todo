63e211ece47ebb814b32ec3583c7916563259e02
Rodrigo Silveira <r3850365@gmail.com>
2020-05-18 10:55:48 -0700

    security  -  Security - Link Todo to their Owners - Pre-Load the database

--- src/main/java/com/madronetek/admin/todo/DatabaseLoader.java
+++ src/main/java/com/madronetek/admin/todo/DatabaseLoader.java
@@ -10,25 +10,34 @@ import org.springframework.stereotype.Component;
 // It implements Spring Boot’s CommandLineRunner so that it gets run after all the beans are created and registered.
 public class DatabaseLoader implements CommandLineRunner {
 
-    private final TodoRepository repository;
+    private final TodoRepository todos;
+    private final OwnerRepository owners;
 
     // It uses constructor injection and autowiring to get Spring Data’s automatically created TodoRepository.
     @Autowired
-    public DatabaseLoader(TodoRepository repository) {
-        this.repository = repository;
+    public DatabaseLoader(TodoRepository repository, OwnerRepository ownerRepository) {
+
+        this.todos = repository;
+        this.owners = ownerRepository;
     }
 
     @Override
     // The run() method is invoked with command line arguments, loading up your data.
-    public void run(String... strings) throws Exception {
-        this.repository.save(new Todo("Learn Springboot", Priorities.LOW));
-        this.repository.save(new Todo("Learn Akka", Priorities.HIGH));
-        this.repository.save(new Todo("Swim 50 laps in 30 minutes", Priorities.LOW));
-        this.repository.save(new Todo("Exercise on the TotalGym 3 times a week", Priorities.LOW));
-        this.repository.save(new Todo("Pedal on the recumbent 3 times a week", Priorities.HIGH));
-        this.repository.save(new Todo("Cook fish twice as week", Priorities.LOW));
-        this.repository.save(new Todo("Measure blood pressure daily", Priorities.LOW));
-        this.repository.save(new Todo("Measure peek flow daily", Priorities.LOW));
-        this.repository.save(new Todo("Measure oxygen level daily", Priorities.LOW));
+    public void run(String... strings) {
+
+        // Owners
+        Owner donald = this.owners.save(new Owner("donald", "trump"));
+        Owner nancy = this.owners.save(new Owner("nancy", "pelosi"));
+
+        //  Todos
+        this.todos.save(new Todo("Learn Springboot", Priorities.LOW, donald));
+        this.todos.save(new Todo("Learn Akka", Priorities.HIGH, nancy));
+        this.todos.save(new Todo("Swim 50 laps in 30 minutes", Priorities.LOW, donald));
+        this.todos.save(new Todo("Exercise on the TotalGym 3 times a week", Priorities.LOW, nancy));
+        this.todos.save(new Todo("Pedal on the recumbent 3 times a week", Priorities.HIGH, donald));
+        this.todos.save(new Todo("Cook fish twice as week", Priorities.LOW, nancy));
+        this.todos.save(new Todo("Measure blood pressure daily", Priorities.LOW, donald));
+        this.todos.save(new Todo("Measure peek flow daily", Priorities.LOW, nancy));
+        this.todos.save(new Todo("Measure oxygen level daily", Priorities.LOW, donald));
     }
 }
