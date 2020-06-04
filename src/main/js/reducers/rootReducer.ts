ef19b55d8ccda39fc5507995bc2a573630676850
Rodrigo Silveira <r3850365@gmail.com>
2020-06-04 13:22:11 -0700

    master-ts-01-basic  -  Migrate to Typescript / Redux MUI: - Logic to manage the page size preferences

--- src/main/js/reducers/rootReducer.ts
+++ src/main/js/reducers/rootReducer.ts
@@ -2,11 +2,17 @@ import { combineReducers } from 'redux'
 import { todo_reducer } from './todo-reducer'
 import { authenticated_user_reducer} from "./authenticted_user-reducer";
 import { visibility_filter_reducer } from './visibility-filter-reducer'
+import { rest_parameter_attributes_reducer } from './rest-parameter-reducer'
+import { rest_parameter_page_size_reducer } from './rest-parameter-reducer'
+import { rest_parameter_links_reducer } from './rest-parameter-reducer'
 
 export const rootReducer = combineReducers({
 	todo_reducer,
 	authenticated_user_reducer,
-	visibility_filter_reducer
+	visibility_filter_reducer,
+	rest_parameter_attributes_reducer,
+	rest_parameter_page_size_reducer,
+	rest_parameter_links_reducer
 })
 
 export type RootState = ReturnType<typeof rootReducer>
