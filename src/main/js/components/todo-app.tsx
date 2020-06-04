fed6485fc73b1e4b5eacd1cf79ca9c40162e0d02
Rodrigo Silveira <r3850365@gmail.com>
2020-06-04 13:22:49 -0700

    master-ts-01-basic  -  Migrate to Typescript / Redux MUI: - Added the todo-list component

--- src/main/js/components/todo-app.tsx
+++ src/main/js/components/todo-app.tsx
@@ -1,28 +1,27 @@
 // External Dependencies
 import * as React from "react";
-import {useContext} from "react";
+import {useContext, useEffect} from "react";
 import { makeStyles, Theme } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
-import { withLifecycleActions } from 'react-redux-lifecycle';
+// import { withLifecycleActions } from 'react-redux-lifecycle';
 
 // Internal Dependencies
 import { AuthenticationContext } from '../context-providers/autentication-context-provider';
 // import TodoForm from "./todo-form";
-// import TodoList from "./todo-list";
+import TodoList from "./todo-list";
 // import VisibilityFilters from "./visibility-filters";
 // import "../styles/styles.css";
-
-// Internal dependencies
 import { RootState } from '../reducers/rootReducer'
 import {connect} from 'react-redux';
 // import TodoItem from './todo-item'
 // import { TodoInterface } from "../interfaces/interfaces";
 // import {todo_delete, todo_toggle, todo_update} from "../actions/todo-actions";
-
 // import classes from "*.module.scss";
+import { loadFromServer } from "../services/load-from-server";
+
 const useStyles = makeStyles((theme: Theme) => ({
 	root: {
 		flexGrow: 1,
@@ -38,11 +37,6 @@ const useStyles = makeStyles((theme: Theme) => ({
 	}
 }));
 
-const readTodos = () => {
-	console.log('TodoApp/myActionCreator')
-	return { type: 'MY_ACTION' }
-}
-
 /*
  * *****************************************************************************
  * This is the heart of the component
@@ -56,7 +50,8 @@ const readTodos = () => {
 // Set to null if not used
 function mapStateToProps (state: RootState) {
 	return {
-		userName: state.authenticated_user_reducer
+		userName: state.authenticated_user_reducer,
+		pageSize: state.rest_parameter_page_size_reducer
 	};
 }
 
@@ -83,7 +78,11 @@ const TodoApp = (props: Props) => {
 	console.log('TodoApp: Loading the app')
 	const classes = useStyles();
 	const userName = useContext(AuthenticationContext)
-	console.log("TodoApp: User name: " + userName);
+	console.log("TodoApp: User name: " + props.userName);
+	useEffect(() => {
+		loadFromServer(props.pageSize);
+	});
+	
 	return (
 		<div className="todo-list-app">
 			{/* <h1 className="todo-header">todo</h1>*/}
@@ -107,10 +106,7 @@ const TodoApp = (props: Props) => {
 			{/*< CreateTodoItemForm />*/}
 			{/*< SelectPageSizeForm />*/}
 			{/*< TodoForm />*/}
-			{/*<TodoList*/}
-			{/*	// todoList={todoList}*/}
-			{/*	// visibilityFilter={visibilityFilter}*/}
-			{/*/>*/}
+			<TodoList/>
 			{/*<VisibilityFilters />*/}
 		</div>
 	)
---
+++ src/main/js/components/todo-list.tsx
@@ -0,0 +1,190 @@
+// External dependencies
+import * as React from 'react'
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+// import TableHead from '@material-ui/core/TableHead';
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+import IconButton from "@material-ui/core/IconButton";
+import DeleteIcon from "@material-ui/icons/Delete";
+import Divider from "@material-ui/core/Divider";
+import Select from '@material-ui/core/Select';
+import MenuItem from '@material-ui/core/MenuItem';
+
+// Internal dependencies
+import { RootState } from '../reducers/rootReducer'
+import {connect} from 'react-redux';
+// import TodoItem from './todo-item'
+import { TodoInterface } from "../interfaces/interfaces";
+// import {todo_delete, todo_toggle, todo_update} from "../actions/todo-actions";
+
+/*
+ * *****************************************************************************
+ * This is the heart of the component
+ * *****************************************************************************
+ */
+
+// Comment out if not used
+// interface OwnProps {
+// }
+
+// Set to null if not used
+function mapStateToProps (state: RootState) {
+	return {
+		todoList: state.todo_reducer.slice(0),
+		visibilityFilter: state.visibility_filter_reducer
+	};
+}
+
+// Set to null if not used
+const mapDispatchToProps = {
+	todo_toggle: (id: string) => todo_toggle(id),
+	todo_update: (id: string, text: string, priority: string) => todo_update(id, text, priority),
+	todo_delete: (text: string) => todo_delete(text)
+}
+
+// Hook them up; note that the static typing is constrained to what is in use
+const connector = connect(
+	mapStateToProps,
+	mapDispatchToProps
+)
+type StateProps = ReturnType<typeof mapStateToProps>;
+type DispatchProps = typeof mapDispatchToProps
+type Props = StateProps & DispatchProps
+// type Props = StateProps & DispatchProps & OwnProps;
+
+/*
+ * *****************************************************************************
+ * End of the heart of the component
+ * *****************************************************************************
+ */
+
+/*
+ * *****************************************************************************
+ * Temporary ... replace with actions
+ * *****************************************************************************
+ */
+const todo_toggle = (id: string) => {
+ 	console.log('TodoList/todo_toggle id = ' + id);
+}
+const todo_update = (id: string, text: string, priority: string) => {
+	console.log('TodoList/todo_update id = ' + id + ', text = ' + text + ', priority = ' + priority);
+}
+const todo_delete = (id: string) => {
+	console.log('TodoList/todo_delete id = ' + id);
+}
+
+function computeVisible (visibilityFilter: string, isCompleted: boolean ): string {
+	// console.log('TodoList/computeVisible visibilityFilter: ' + visibilityFilter)
+	let className = 'show-todo-item';
+	if ((visibilityFilter === 'open' && isCompleted) || (visibilityFilter === 'done' && !isCompleted)) {
+		className = 'hide-todo-item';
+	}
+	return className;
+}
+
+const useStyles = makeStyles({
+	table: {
+		/* minWidth: 650, */
+		tableLayout: "auto",
+		width: "100%",
+		'& .hide-todo-item': {
+			display: 'none',
+		},
+	},
+	todoTextFont: {
+		fontSize: "1rem",
+	},
+	todoPriority: {
+		width: "32px"
+	},
+	todoPriorityFont: {
+		fontSize: "1rem"
+	}
+});
+
+const computeState = (isCompleted: boolean) => {
+	return isCompleted ? (
+		<span className="todo-item-state todo-item-checked">✔</span>
+	) : (
+		<span className="todo-item-state todo-item-unchecked" />
+	);
+};
+
+// TodoList component
+const TodoList = (props: Props) => {
+	const classes = useStyles();
+	
+	return (
+		<div>
+			<Divider />
+			<TableContainer component={Paper}>
+				<Table className={classes.table} size="small" aria-label="a dense table">
+					<TableBody>
+						{
+							props.todoList.length > 0
+								?
+								(
+									props.todoList.map((todo: TodoInterface) => (
+										<TableRow key={todo.id} className={computeVisible(props.visibilityFilter, todo.isCompleted)}>
+											<TableCell
+												component="th"
+												scope="row"
+												className={"todo-is-completed"}
+												onClick={() => props.todo_toggle(todo.id)}
+											>
+												{computeState(todo.isCompleted)}
+											</TableCell>
+											<TableCell className={classes.todoTextFont}>
+												<TextField
+													id="standard-basic"
+													value={todo.text}
+													onChange={() => props.todo_update(todo.id, todo.text, todo.priority)}
+													fullWidth />
+											</TableCell>
+											<TableCell className={classes.todoPriority}>
+												<Select className={classes.todoPriorityFont}
+														labelId="demo-simple-select-label"
+														id="demo-simple-select"
+														value={todo.priority}
+														onChange={(event: React.ChangeEvent<{ value: unknown }>) => props.todo_update(todo.id, todo.text, event.target.value as string)}
+												>
+													<MenuItem value={'LOW'}>LOW</MenuItem>
+													<MenuItem value={'MEDIUM'}>MEDIUM</MenuItem>
+													<MenuItem value={'HIGH'}>HIGH</MenuItem>
+												</Select>
+											</TableCell>
+											<TableCell align="right" className={"todo-delete-me"}>
+												<IconButton
+													aria-label="delete"
+													onClick={() => props.todo_delete(todo.id)}
+												>
+													<DeleteIcon fontSize="small" />
+												</IconButton>
+											</TableCell>
+										</TableRow>
+									))
+								)
+								:
+								(
+									<TableRow key='NONE'>
+										{/*<TableCell component="th" scope="row" className={"todo-is- completed"}> </TableCell>*/}
+										<TableCell className={"todo-item"} align={'center'}>
+											<div>You are done</div>
+										</TableCell>
+										{/*<TableCell align="right" className={"todo-delete-me"}> </TableCell>*/}
+									</TableRow>
+								)
+						}
+					</TableBody>
+				</Table>
+			</TableContainer>
+		</div>
+	)
+}
+
+export default connector(TodoList)
