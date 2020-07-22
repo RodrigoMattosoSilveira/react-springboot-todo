e43c4ba09db0c90ca40b632973f3c3374a6d2271
Rodrigo Silveira <r3850365@gmail.com>
2020-07-21 19:36:08 -0700

    websocket-implementation  -  Migrate to Typescript / Redux MUI: - return to the starting page when reseting the message badge

--- src/main/js/actions/websocket-actions.ts
+++ src/main/js/actions/websocket-actions.ts
@@ -1,6 +1,6 @@
 import {REST_ACTIONS, WEBSOCKET_ACTIONS} from '../references/references';
 import {IWebSocketMessage, RestAttributesActionInterface} from '../references/interfaces';
-import {todo_navigate_to_page_thunk} from "./todos-actions";
+import {todo_load_from_server, todo_navigate_to_page_thunk} from "./todos-actions";
 
 
 export const websocket_add_message_action = (message: IWebSocketMessage): IWebSocketMessage => ({
@@ -17,7 +17,8 @@ export const  websocket_remove_messages_thunk = (navUri: string, pageNumber: num
 	(dispatch: any, getState: any, axios: any) => {
 		console.log('websocket_remove_messages_thunk/navUri: ' + navUri);
 		dispatch(websocket_remove_messages());
-		dispatch(todo_navigate_to_page_thunk(navUri, pageNumber));
+		// dispatch(todo_navigate_to_page_thunk(navUri, pageNumber));
+		dispatch(todo_load_from_server());
 	}
 
 export const  websocket_new_todo_message_thunk = (message: any) =>
